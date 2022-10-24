180a243dfd5fc88331c77357efc20b481b9285af
HALLEY121 <1343325715@qq.com>
2022-10-24 23:32:27 +0800

    first r

---
+++ mnt/os/target/debug/deps/os-c4e64d229733e3a7.d
@@ -0,0 +1,5 @@
+/mnt/mnt/os/target/debug/deps/os-c4e64d229733e3a7: src/main.rs
+
+/mnt/mnt/os/target/debug/deps/os-c4e64d229733e3a7.d: src/main.rs
+
+src/main.rs:
---
+++ mnt/os/target/debug/os.d
@@ -0,0 +1,1 @@
+/mnt/mnt/os/target/debug/os: /mnt/mnt/os/src/main.rs
